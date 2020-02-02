b9cee0a2a4091ec2730372f20752473d427615f2
Jordi Santiago <metalzilla@gmail.com>
2020-02-02 13:08:20 +0100

    Titlescreen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public AudioSource MusicSource;
+    public AudioClip IntroClip;
+    public AudioClip IngameClip;
+
+    public CanvasGroup TitleScreen;
+
     public int PlayerCount = 2;
     public Material[] PlayerMaterials;
 
@@ -23,7 +29,9 @@ public class GameManager : MonoBehaviour
 
     PlayerController[] players;
     CameraManager cameraManager;
-    bool reset = false;
+
+    bool BackToTitle = true;
+    bool ResetGame = false;
 
     void Start()
     {
@@ -32,116 +40,175 @@ public class GameManager : MonoBehaviour
 
     IEnumerator Flow()
     {
-        while (true)
+        TitleScreen.alpha = 0.0f;
+
+        while(true)
         {
-            reset = false;
+            MusicSource.clip = IntroClip;
+            MusicSource.Play();
 
-            if (PlayerCount < 2 && PlayerCount > 4)
-                Debug.Break();
-
-            if (players != null)
-                foreach (PlayerController p in players)
-                    if (p != null)
-                        Destroy(p.gameObject);
-
-            foreach (GameObject j in junkList)
-                    Destroy(j);
-
-            junkList.Clear();
-
-            if (cameraManager != null)
+            // Title screen fade
+            if (!FastSpawn) 
             {
-                cameraManager.Destroy();
-                Destroy(cameraManager.gameObject);
-            }
-
-            // CREATE INITIAL JUNK
-
-            int totalJunk = 0;
-            int[] freqAccum = new int[JunkPrefabs.Length];
-            for (int i = 0; i < JunkPrefabs.Length; i++)
-            {
-                totalJunk += JunkPrefabs[i].freq;
-                freqAccum[i] = totalJunk;
-            }
-
-            UnityEngine.Random rand = new UnityEngine.Random();
-            for (int i = 0; i < InitialJunkCount; i++)
-            {
-                int sel = 0;
-                int val = UnityEngine.Random.Range(0, totalJunk);
-                for (; sel < freqAccum.Length && val > freqAccum[sel]; sel++);
-
-                GameObject junk = Instantiate(JunkPrefabs[sel].prefab);
-                junk.name = string.Format("Junk{0}({1})", i, JunkPrefabs[sel].prefab.name);
-                junkList.Add(junk);
-                float pos = (float) (Math.Round(UnityEngine.Random.Range(0.0f, 1.0f)) * 2.0f - 1.0f) * 75.0f;
-                junk.transform.position = new Vector3(pos, 1.0f, 0.0f);
-                junk.transform.rotation *= Quaternion.Euler(0, 0, UnityEngine.Random.Range(0, 2 * Mathf.PI));
-
-                if (!FastSpawn || i % 10 == 0)
+                float time = Time.time;
+                while ((Time.time - time) < 3)
                     yield return null;
+
+                while ((Time.time - time) < 4)
+                {
+                    TitleScreen.alpha = (Time.time - time) - 3.0f;
+                    yield return null;
+                }
             }
 
-            // WAIT 5 SECS
+            // Show title screen
+            TitleScreen.alpha = 1.0f;
+
+            while(BackToTitle)
+                yield return null;
+
+            MusicSource.Stop();
+            MusicSource.clip = IngameClip;
+            MusicSource.Play();
+
             if (!FastSpawn)
             {
                 float time = Time.time;
-                while ((Time.time - time) < 5)
+                while ((Time.time - time) < 1)
+                {
+                    TitleScreen.alpha = 1.0f - (Time.time - time);
+                    yield return null;
+                }
+            }
+
+            // Hide title screen
+            TitleScreen.alpha = 0.0f;
+
+            while (true)
+            {
+                ResetGame = false;
+
+                if (PlayerCount < 2 && PlayerCount > 4)
+                    Debug.Break();
+
+                if (players != null)
+                    foreach (PlayerController p in players)
+                        if (p != null)
+                            Destroy(p.gameObject);
+
+                foreach (GameObject j in junkList)
+                    Destroy(j);
+
+                junkList.Clear();
+
+                if (cameraManager != null)
+                {
+                    cameraManager.Destroy();
+                    Destroy(cameraManager.gameObject);
+                }
+
+                if (BackToTitle)
+                    break;
+
+                // CREATE INITIAL JUNK
+
+                int totalJunk = 0;
+                int[] freqAccum = new int[JunkPrefabs.Length];
+                for (int i = 0; i < JunkPrefabs.Length; i++)
+                {
+                    totalJunk += JunkPrefabs[i].freq;
+                    freqAccum[i] = totalJunk;
+                }
+
+                UnityEngine.Random rand = new UnityEngine.Random();
+                for (int i = 0; i < InitialJunkCount; i++)
+                {
+                    int sel = 0;
+                    int val = UnityEngine.Random.Range(0, totalJunk);
+                    for (; sel < freqAccum.Length && val > freqAccum[sel]; sel++) ;
+
+                    GameObject junk = Instantiate(JunkPrefabs[sel].prefab);
+                    junk.name = string.Format("Junk{0}({1})", i, JunkPrefabs[sel].prefab.name);
+                    junkList.Add(junk);
+                    float pos = (float)(Math.Round(UnityEngine.Random.Range(0.0f, 1.0f)) * 2.0f - 1.0f) * 75.0f;
+                    junk.transform.position = new Vector3(pos, 1.0f, 0.0f);
+                    junk.transform.rotation *= Quaternion.Euler(0, 0, UnityEngine.Random.Range(0, 2 * Mathf.PI));
+
+                    if (!FastSpawn || i % 10 == 0)
+                        yield return null;
+                }
+
+                // WAIT 5 SECS
+                if (!FastSpawn)
+                {
+                    float time = Time.time;
+                    while ((Time.time - time) < 5)
+                        yield return null;
+                }
+
+                // CREATE PLAYERS
+                players = new PlayerController[PlayerCount];
+
+                Camera CameraTemplate = transform.Find("CameraTemplate")?.GetComponent<Camera>();
+                if (CameraTemplate == null)
+                    Debug.Break();
+
+                PlayerController PlayerTemplate = transform.Find("PlayerTemplate")?.GetComponent<PlayerController>();
+                if (PlayerTemplate == null)
+                    Debug.Break();
+
+                for (int i = 0; i < PlayerCount; i++)
+                {
+                    // Create player
+                    players[i] = Instantiate(PlayerTemplate);
+                    players[i].gameObject.SetActive(true);
+                    players[i].gameObject.name = string.Format("Player{0}", i + 1);
+                    players[i].Init(i);
+
+                    float side = ((i % 2) * 2) - 1.0f;
+                    float team = ((i / 2) * 2) - 1.0f;
+                    players[i].transform.position = new Vector3(50.0f * side + 3.0f * team, 0, 0);
+
+                    foreach (MeshRenderer plRender in players[i].ColorRenderers)
+                        if (plRender && PlayerMaterials != null && i < PlayerMaterials.Length)
+                            plRender.material = PlayerMaterials[i];
+                }
+
+                if (PlayerCount == 2)
+                {
+                    var camDirectorObj = new GameObject();
+                    camDirectorObj.name = "CameraDirector";
+                    cameraManager = camDirectorObj.AddComponent<CameraDirector>();
+                    cameraManager.Setup(CameraTemplate, players);
+                }
+                else
+                {
+                    var camDirectorObj = new GameObject();
+                    camDirectorObj.name = "CameraSplit";
+                    cameraManager = camDirectorObj.AddComponent<CameraSplit>();
+                    cameraManager.Setup(CameraTemplate, players);
+                }
+
+                while (!ResetGame && !BackToTitle)
                     yield return null;
             }
-
-            // CREATE PLAYERS
-            players = new PlayerController[PlayerCount];
-
-            Camera CameraTemplate = transform.Find("CameraTemplate")?.GetComponent<Camera>();
-            if (CameraTemplate == null)
-                Debug.Break();
-
-            PlayerController PlayerTemplate = transform.Find("PlayerTemplate")?.GetComponent<PlayerController>();
-            if (PlayerTemplate == null)
-                Debug.Break();
-
-            for (int i = 0; i < PlayerCount; i++)
-            {
-                // Create player
-                players[i] = Instantiate(PlayerTemplate);
-                players[i].gameObject.SetActive(true);
-                players[i].gameObject.name = string.Format("Player{0}", i + 1);
-                players[i].Init(i);
-
-                float side = ((i % 2) * 2) - 1.0f;
-                float team = ((i / 2) * 2) - 1.0f;
-                players[i].transform.position = new Vector3(50.0f * side + 3.0f * team, 0, 0);
-
-                foreach(MeshRenderer plRender in players[i].ColorRenderers)
-                    if (plRender && PlayerMaterials != null && i < PlayerMaterials.Length)
-                        plRender.material = PlayerMaterials[i];
-            }
-
-            if (PlayerCount == 2)
-            {
-                var camDirectorObj = new GameObject();
-                camDirectorObj.name = "CameraDirector";
-                cameraManager = camDirectorObj.AddComponent<CameraDirector>();
-                cameraManager.Setup(CameraTemplate, players);
-            }
-            else
-            {
-                var camDirectorObj = new GameObject();
-                camDirectorObj.name = "CameraSplit";
-                cameraManager = camDirectorObj.AddComponent<CameraSplit>();
-                cameraManager.Setup(CameraTemplate, players);
-            }
-
-            while(!reset)
-                yield return null;
         }
     }
 
     void OnGUI()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-            reset = true;
+        if (BackToTitle)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                BackToTitle = false;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+                ResetGame = true;
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+                BackToTitle = true;
+        }
     }
 }
---
+++ Assets/Scripts/UI/Shake.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{ 
+    RectTransform rect;
+    Vector2 initAnchoredPos;
+
+    void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        initAnchoredPos = rect.anchoredPosition;
+    }
+
+    void Update()
+    {
+        rect.anchoredPosition = initAnchoredPos + new Vector2(Random.Range(-8, 8), Random.Range(-8, 8));
+    }
+}
